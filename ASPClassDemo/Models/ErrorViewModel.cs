baf00ab53fa23799824eb8c3e8f4cb0e406fa9df
Taj <tajmarie92@gmail.com>
2023-11-08 10:17:23 -0600

    Add project files.

---
+++ ASPClassDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASPClassDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
